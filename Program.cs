880b7adcbb777531f4b0ac5d0c9f9fb2588c69df
jbarus <jakubbarus@gmail.com>
2022-11-20 18:04:18 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _1._Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] result;
+            int[] question = {3, 3};
+            result = TwoSum(question,6);
+        }
+
+        static public int[] TwoSum(int[] nums, int target) {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target && i !=j) {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }  
+            return result;
+        }
+    }
+}
